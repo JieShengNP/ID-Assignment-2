d2832b61f872c0d83c29d57c9c021ac1f22d94ce
JieShengNP <s10205049@connect.np.edu.sg>
2021-01-08 14:55:29 +0800

    Added Sign Up - Data Creation in Database

--- js/script.js
+++ js/script.js
@@ -7,50 +7,118 @@ var usernameSettings = {
         "content-type": "application/json",
         "x-apikey": "5ff2b985823229477922c6e2",
         "cache-control": "no-cache"
-        }
     }
-    
-    $.ajax(usernameSettings).done(function (response) {
-        console.log(response);
+}
+
+$.ajax(usernameSettings).done(function (response) {
+    console.log(response);
 });
-
+var db = new restdb("5ff2b985823229477922c6e2");
 var userID = null;
+var userFID = null;
+var userAID = null;
+var existingUsername = null;
 
-function signupGetValue(){
-    var signupUsername = document.getElementById("su-username").value;
-    var signupEmail = document.getElementById("su-email").value;
+function signupGetValue() {
+    var signupUsername = document.getElementById("su-username").value.toUpperCase();
+    var signupEmail = document.getElementById("su-email").value.toUpperCase();
     var signupPassword = document.getElementById("su-password").value;
-    $.ajax(usernameSettings).done(function(response) {
-        for(i = 0; i < response.length; i++){
-            if (signupUsername == response[i].Username){
-                console.log("Username already exist!");
-                return;
-            } else if (signupEmail == response[i].AccountDetails[0].Email) {
-                console.log("Email already exist!");
-                return;
+    $.ajax(usernameSettings).done(function (response) {
+        for (i = 0; i < response.length; i++) {
+            if (signupUsername == response[i].Username) {
+                $('p#su-un-taken').css("opacity", "1");
+                existingUsername = signupUsername;
+                var checkUsername = setInterval(function () {
+                    if (document.getElementById("su-username").value.toUpperCase() != existingUsername) {
+                        $('p#su-un-taken').css("opacity", "0");
+                        clearInterval(checkUsername);
+                    }
+                }, 100);
+                break;
             }
-            if (i+1 == response.length){
-                // #TODO: Create Account
-                console.log("Create New Account");
+            if (signupEmail == response[i].AccountDetails[0].Email) {
+                $('p#su-em-taken').css("opacity", "1");
+                existingEmail = signupEmail;
+                var checkEmail = setInterval(function () {
+                    if (document.getElementById("su-email").value.toUpperCase() != existingEmail) {
+                        $('p#su-em-taken').css("opacity", "0");
+                        clearInterval(checkEmail);
+                    }
+                }, 100);
+                break;
+            }
+            if (i + 1 == response.length) {
+                //Create Accounts Database
+                var ajaxSettings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://financialtracker-407b.restdb.io/rest/accounts",
+                    "method": "POST",
+                    "headers": {
+                        "x-apikey": '5ff2b985823229477922c6e2',
+                        "content-type": "application/json"
+                    },
+                    "processData": false,
+                    "data": JSON.stringify({ Email: signupEmail, Password: signupPassword })
+                }
+                $.ajax(ajaxSettings).done(function (response) {
+                    console.log(response);
+                    window.userAID = response._id;
+                    //Create Default Finance Database
+                    var ajaxSettings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": "https://financialtracker-407b.restdb.io/rest/finance",
+                        "method": "POST",
+                        "headers": {
+                            "x-apikey": '5ff2b985823229477922c6e2',
+                            "content-type": "application/json"
+                        },
+                        "processData": false,
+                        "data": JSON.stringify({Transport: 0, Shopping: 0, Entertainment: 0, Food: 0, Others: 0})
+                    }
+                    $.ajax(ajaxSettings).done(function (response) {
+                        console.log(response);
+                        window.userFID = response._id;
+                        //Create Usernames Database to contain everything
+                        var ajaxSettings = {
+                            "async": true,
+                            "crossDomain": true,
+                            "url": "https://financialtracker-407b.restdb.io/rest/usernames",
+                            "method": "POST",
+                            "headers": {
+                                "x-apikey": '5ff2b985823229477922c6e2',
+                                "content-type": "application/json"
+                            },
+                            "processData": false,
+                            "data": JSON.stringify({Username: signupUsername, AccountDetails:[userAID], FinanceInfo:[userFID]})
+                        }
+                        $.ajax(ajaxSettings).done(function (response) {
+                            console.log(response);
+                            window.userFID = response._id;
+                        })
+
+                    })
+                })
             }
         }
     });
 }
 
-function signinGetValue(){
-    function accountCheckFunction(item, index) {
-        if (signupUsername == item.Username && signupPassword == item.AccountDetails[0].Password){
-            console.log("Success");
-            window.userID = index;
-            window.localStorage.setItem("AccountInfo", String(item));
-            return;
-        } else {
-            console.log("Fail");
-        }
-    }
-    var signupUsername = document.getElementById("su-username").value;
+function signinGetValue() {
+    var signupUsername = document.getElementById("su-username").value.toUpperCase();
     var signupPassword = document.getElementById("su-password").value;
     $.ajax(usernameSettings).done(function (response) {
-        response.forEach(accountCheckFunction);
+        for (i = 0; i < response.length; i++) {
+            if (signupUsername == response[i].Username && signupPassword == response[i].AccountDetails[0].Password) {
+                console.log("Success");
+                window.userID = response[i]._id;
+                window.localStorage.removeItem("AccountInfo");
+                window.localStorage.setItem("AccountInfo", JSON.stringify(response[i]));
+                break;
+            } else {
+                console.log("Fail");
+            }
+        }
     });
 }
